5761f27ec77810e428881880c8f8a9d08d3834c3
tewaker33 <wijnsmalaurens@gmail.com>
2018-01-23 12:11:25 +0100

    created splitpane added 2e pie

---
+++ nl/SeriousParking/Parkeersimulator/view/PieChartView2.java
@@ -0,0 +1,54 @@
+package nl.SeriousParking.Parkeersimulator.view;
+
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.VBox;
+import nl.SeriousParking.Parkeersimulator.controller.SimulatorController;
+import nl.SeriousParking.Parkeersimulator.model.Simulator;
+
+
+public class PieChartView2 extends View<SimulatorController, Simulator> {
+    PieChart.Data abbonementSlice;
+    PieChart.Data adhocSlice;
+    PieChart.Data dubbelpSlice;
+    PieChart.Data gereserveerdSlice;
+    PieChart.Data VrijePlekkenSlice;
+
+    public PieChartView2(SimulatorController controller, Simulator model) {
+        super(controller, model);
+
+        PieChart pieChart = new PieChart();
+
+        abbonementSlice = new PieChart.Data("Abbonoment", model.getNumberOfPasscarsinPark());
+        adhocSlice = new PieChart.Data("AdHoc", model.getNumberOfAddhoccarsinPark());
+        dubbelpSlice = new PieChart.Data("Dubbelparkeerder", model.getNumberOfCarsParkedDouble());
+        VrijePlekkenSlice = new PieChart.Data("Vrije Plekken", model.getNumberOfOpenSpots());
+        gereserveerdSlice= new PieChart.Data("Gereserveerd", 0);
+
+        pieChart.getData().add(abbonementSlice);
+        pieChart.getData().add(adhocSlice);
+        pieChart.getData().add(dubbelpSlice);
+        pieChart.getData().add(gereserveerdSlice);
+        pieChart.getData().add(VrijePlekkenSlice);
+        pieChart.setLegendVisible(false);
+        pieChart.setClockwise(true);
+        pieChart.setStartAngle(90);
+
+        VBox pie = new VBox(pieChart);
+        this.getChildren().add(pie);
+        model.addView(this);
+
+    }
+
+    @Override
+    public void update() {
+        abbonementSlice.setPieValue(model.getNumberOfPasscarsinPark());
+        adhocSlice.setPieValue(model.getNumberOfAddhoccarsinPark());
+        dubbelpSlice.setPieValue(model.getNumberOfCarsParkedDouble());
+        gereserveerdSlice.setPieValue(0);
+        VrijePlekkenSlice.setPieValue(model.getNumberOfOpenSpots());
+
+    }
+
+
+}
--- nl/SeriousParking/Parkeersimulator/view/RootView.java
+++ nl/SeriousParking/Parkeersimulator/view/RootView.java
@@ -20,13 +20,18 @@ public class RootView {
     public void RootView(Stage primaryStage) {
         primaryStage.setTitle("SeriousParking Parkeersimulator");
 
-        SplitPane splitPane = new SplitPane();
+        SplitPane splitPanebottom = new SplitPane();
+        SplitPane splitPanetop = new SplitPane();
 
-        Scene scene = new Scene(splitPane,1500,900);
+        Scene scene = new Scene(splitPanebottom,1500,900);
 
-        splitPane.setOrientation(Orientation.VERTICAL);
-        splitPane.prefWidthProperty().bind(scene.widthProperty());
-        splitPane.prefHeightProperty().bind(scene.heightProperty());
+        splitPanebottom.setOrientation(Orientation.VERTICAL);
+        splitPanebottom.prefWidthProperty().bind(scene.widthProperty());
+        splitPanebottom.prefHeightProperty().bind(scene.heightProperty());
+
+        splitPanetop.setOrientation(Orientation.HORIZONTAL);
+        splitPanetop.prefWidthProperty().bind(scene.widthProperty());
+        splitPanetop.prefHeightProperty().bind(scene.heightProperty());
 
         Simulator model                 = new Simulator();
         SimulatorController controller  = new SimulatorController(model);
@@ -40,12 +45,14 @@ public class RootView {
         scrollPane.setFitToHeight(true);
 
         PieChartView piechart = new PieChartView(controller, model);
+        PieChartView2 piechart2 = new PieChartView2(controller, model);
 
         SimData legend = new SimData(controller, model);
 
         TextView textview  = new TextView(controller, model);
 
         TabPane tabPane         = new TabPane();
+        TabPane pie         = new TabPane();
         BorderPane borderPane   = new BorderPane();
         Button start            = new Button("Start/Stop");
         Button reset            = new Button("Reset");
@@ -66,28 +73,39 @@ public class RootView {
 
 
         toolBar.getItems().addAll(start,reset,tick);
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        pie.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
 
         Tab TextViewTab = new Tab();
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         TextViewTab.setText("Text View");
         TextViewTab.setContent(textview);
         tabPane.getTabs().add(TextViewTab);
 
         Tab SimSettings = new Tab();
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         SimSettings.setText("Settings");
         SimSettings.setContent(scrollPane);
         tabPane.getTabs().add(SimSettings);
+
+        Tab PieChart1 = new Tab();
+        PieChart1.setText("PieChart1");
+        PieChart1.setContent(piechart);
+        pie.getTabs().add(PieChart1);
+
+        Tab PieChart2 = new Tab();
+        PieChart2.setText("PieChart2");
+        PieChart2.setContent(piechart2);
+        pie.getTabs().add(PieChart2);
 
         legend.setAlignment(Pos.CENTER);
 
         borderPane.setLeft(view);
         borderPane.setCenter(legend);
-        borderPane.setRight(piechart);
         borderPane.setBottom(toolBar);
-        borderPane.prefWidthProperty().bind(scene.widthProperty());
 
-        splitPane.getItems().addAll(borderPane,tabPane);
+        splitPanetop.getItems().addAll(borderPane,pie);
+
+        splitPanebottom.getItems().addAll(splitPanetop,tabPane);
 
         primaryStage.setScene(scene);
         scene.getStylesheets().add("Chart.css");

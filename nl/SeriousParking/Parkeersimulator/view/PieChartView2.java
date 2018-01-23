9143a86f017ef4c2364088e4490ef5e6883e8974
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2018-01-23 12:51:27 +0100

    more minor changes

--- nl/SeriousParking/Parkeersimulator/view/PieChartView2.java
+++ nl/SeriousParking/Parkeersimulator/view/PieChartView2.java
@@ -10,18 +10,16 @@ import nl.SeriousParking.Parkeersimulator.model.Simulator;
 public class PieChartView2 extends View<SimulatorController, Simulator> {
     PieChart.Data abbonementSlice;
     PieChart.Data adhocSlice;
-    PieChart.Data dubbelpSlice;
-    PieChart.Data gereserveerdSlice;
-    PieChart.Data VrijePlekkenSlice;
-    double numberofCars;
+
+    double percentage;
     public PieChartView2(SimulatorController controller, Simulator model) {
         super(controller, model);
 
         PieChart pieChart = new PieChart();
 
-        numberofCars=100;
-        abbonementSlice = new PieChart.Data("Abbonoment", (model.getNumberOfPasscarsinPark()/numberofCars*100));
-        adhocSlice = new PieChart.Data("AdHoc", (model.getNumberOfAddhoccarsinPark()/numberofCars*100));
+        percentage=100;
+        abbonementSlice = new PieChart.Data("Abbonoment", (50/percentage*100));
+        adhocSlice = new PieChart.Data("AdHoc", (50/percentage*100));
 
 
 
@@ -41,8 +39,8 @@ public class PieChartView2 extends View<SimulatorController, Simulator> {
     @Override
     public void update() {
 
-        abbonementSlice.setPieValue((model.getNumberOfPasscarsinPark()/numberofCars*100));
-        adhocSlice.setPieValue((model.getNumberOfAddhoccarsinPark()/numberofCars*100));
+        abbonementSlice.setPieValue((model.getNumberOfPasscarsinPark()/percentage*100));
+        adhocSlice.setPieValue((model.getNumberOfAddhoccarsinPark()/percentage*100));
     }
 
 

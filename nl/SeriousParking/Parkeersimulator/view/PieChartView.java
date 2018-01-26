396bba29077cb720a88c08d89fe93ffe52fd26dc
tewaker33 <wijnsmalaurens@gmail.com>
2018-01-26 12:26:06 +0100

    Revert "Merge branch 'master' of https://github.com/AngelsDustz/SeriousParking"
    This reverts commit 7b460140064e68be4d6653bdedbb499787e2804c, reversing
    changes made to f642284a0658cf71cb778adcc9fea78128f4f533.

--- nl/SeriousParking/Parkeersimulator/view/PieChartView.java
+++ nl/SeriousParking/Parkeersimulator/view/PieChartView.java
@@ -7,25 +7,25 @@ import nl.SeriousParking.Parkeersimulator.controller.SimulatorController;
 import nl.SeriousParking.Parkeersimulator.model.Simulator;
 
 
-public class PieChartView extends View<SimulatorController, Simulator> implements Runnable {
+public class PieChartView extends View<SimulatorController, Simulator> {
     PieChart.Data carSlice;
     PieChart.Data empty;
     PieChart.Data dubbelpSlice;
     PieChart.Data gereserveerdSlice;
     PieChart.Data VrijePlekkenSlice;
     double percentofCars;
-    boolean run = false;
 
     public PieChartView(SimulatorController controller, Simulator model) {
         super(controller, model);
 
-        PieChart pieChart   = new PieChart();
-        percentofCars       = 100;
-        carSlice            = new PieChart.Data("Auto's Geparkeerd", 30);
-        empty               = new PieChart.Data("", 0);
-        dubbelpSlice        = new PieChart.Data("Dubbelparkeerder", 10);
-        gereserveerdSlice   = new PieChart.Data("Gereserveerd", 10);
-        VrijePlekkenSlice   = new PieChart.Data("Vrije Plekken", 10);
+        PieChart pieChart = new PieChart();
+        //TODO kleuren refactoren
+        percentofCars=100;
+        carSlice = new PieChart.Data("Auto's Geparkeerd", 30);
+        empty= new PieChart.Data("", 0);
+        dubbelpSlice = new PieChart.Data("Dubbelparkeerder", 10);
+        gereserveerdSlice= new PieChart.Data("Gereserveerd", 10);
+        VrijePlekkenSlice = new PieChart.Data("Vrije Plekken", 10);
 
         pieChart.getData().add(carSlice);
         pieChart.getData().add(empty);
@@ -41,36 +41,16 @@ public class PieChartView extends View<SimulatorController, Simulator> implement
         this.getChildren().add(pie);
         model.addView(this);
 
-        new Thread(this, "PieChartView").start();
-        run = true;
     }
 
     @Override
     public void update() {
+        carSlice.setPieValue(((model.getNumberOfPasscarsinPark()+model.getNumberOfAddhoccarsinPark())/percentofCars*100));
+        dubbelpSlice.setPieValue(model.getNumberOfCarsParkedDouble());
+        gereserveerdSlice.setPieValue(0);
+        VrijePlekkenSlice.setPieValue(model.getNumberOfOpenSpots());
+
     }
 
-    @Override
-    public void run() {
-        while (run) {
-            Double adhocCars    = model.getNumberOfAddhoccarsinPark();
-            Double passCars     = model.getNumberOfPasscarsinPark();
-            Double percentCars  = ((adhocCars + passCars) / 100) * 100;
 
-            Double dubbleCars   = model.getNumberOfCarsParkedDouble();
-            Double reservedCars = 0.0;
-            Double freeSpots    = model.getNumberOfOpenSpots();
-
-            carSlice.setPieValue(percentCars);
-            dubbelpSlice.setPieValue(dubbleCars);
-            gereserveerdSlice.setPieValue(reservedCars);
-            VrijePlekkenSlice.setPieValue(freeSpots);
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
-
